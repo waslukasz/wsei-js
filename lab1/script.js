9e06624dd5afdb586bc18293c414a9397e113578
waslukasz <ljwasiu@gmail.com>
2023-12-01 18:49:24 +0100

    lab1 update

--- lab1/script.js
+++ lab1/script.js
@@ -1,5 +1,5 @@
 const app = document.querySelector("#calc");
-const DEFAULT_CALC_FIELDS = 4;
+const DEFAULT_CALC_FIELDS = 3;
 const ENABLE_DYNAMIC_UPDATE = true;
 
 InitializeCalc();
@@ -30,20 +30,20 @@ function InitializeCalc() {
     results.classList.add("calc-results");
     
     const sum = document.createElement("span");
-    sum.innerText = "Sum: 0";
+    sum.innerText = "Sum: -";
     results.append(sum);
 
-    const difference = document.createElement("span");
-    difference.innerText = "Difference: 0";
-    results.append(difference)
+    const average = document.createElement("span");
+    average.innerText = "Average: -"
+    results.append(average);
 
-    const multiply = document.createElement("span");
-    multiply.innerText = "Multiplication: 0";
-    results.append(multiply);
+    const min = document.createElement("span");
+    min.innerText = "Min: -";
+    results.append(min);
 
-    const division = document.createElement("span");
-    division.innerText = "Division: 0";
-    results.append(division);
+    const max = document.createElement("span");
+    max.innerText = "Max: -"
+    results.append(max);
 
     app.append(results);
 
@@ -65,7 +65,6 @@ function InitializeCalc() {
                 UpdateData(true);
             });
         }
-
     }
     
     function RemoveCalcField() {
@@ -74,14 +73,12 @@ function InitializeCalc() {
     }
 
     function UpdateData(dynamicUpdate) {
+        let data = [];
         let sumResult = 0;
-        let differenceResult = 0;
-        let differenceStarted = false;
-        let multiplicationResult = 0;
-        let multiplicationStarted = false;
-        let divisionResult = 0;
-        let divisionStarted = false;
-        let divisionPossible = true;
+        let avgResult;
+        let minResult;
+        let maxResult;
+
         for (let i = 0; i < fields.children.length; i++) {
             const element = fields.children[i];
             let value = Number(element.value);
@@ -90,46 +87,23 @@ function InitializeCalc() {
                 i--;
                 continue;
             }
-            if (dynamicUpdate && isNaN(value)) {
-                element.placeholder = "Enter number.";
-                element.value = "";
-                break;
-            }
-            sumResult += value;
-            if (!differenceStarted) {
-                differenceResult = value;
-                differenceStarted = true;
-            } else {
-                differenceResult -= value;
-            }
-            if (!multiplicationStarted) {
-                multiplicationResult = value;
-                multiplicationStarted = true;
-            } else {
-                multiplicationResult *= value;
-            }
-
-            if (divisionPossible) {
-                if (!divisionStarted) {
-                    if (value == 0) divisionPossible = false;
-                    if (divisionPossible) {
-                        divisionResult = value;
-                        divisionStarted = true;
-                    }
-                } else {
-                    if (value == 0) divisionPossible = false;
-                    if (divisionPossible) {
-                        divisionResult /= value;
-                    }
-                }
-            }
+            if (!element.value == "" && !isNaN(value)) data.push(value);
         }
+        for (let i = 0; i < data.length; i++) {
+            const el = data[i];
+            sumResult += el;
+            if (i == 0) {
+                minResult = el;
+                maxResult = el;
+            }
+            if (minResult > el) minResult = el;
+            if (maxResult < el) maxResult = el;
+        }
+        avgResult = (data.reduce((a, b) => a + b, 0) / data.length);
 
         sum.innerText = `Sum: ${sumResult}`;
-        difference.innerText = `Difference: ${differenceResult}`;
-        multiply.innerText = `Multiplication: ${multiplicationResult}`;
-        if (divisionPossible) division.innerText = `Division: ${divisionResult}`;
-        else division.innerText = "Division not possible.";
-        
+        average.innerText = `Average: ${avgResult}`;
+        min.innerText = `Min: ${minResult}`;
+        max.innerText = `Min: ${maxResult}`;
     }
 }
